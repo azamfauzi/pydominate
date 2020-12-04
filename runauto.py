29b2ea8da84453c899bc5c301913551ecf26bfe9
Azam <ibaneznuke@gmail.com>
2020-12-04 20:03:49 +0800

    update new function

--- runauto.py
+++ runauto.py
@@ -1,13 +1,41 @@
 import dominate
 from dominate.tags import *
-dv = div()
-dv['class'] = 'form-group'
+def generateLabel() :
+    lblname = input("Please Insert Label Name")
+    x = label(lblname,cls="col-md-2 control-label")
+    return x
+def generateInput():
+    inputname = input("Please Insert Input Name")
+    c = input_(name=inputname,id=inputname)
+    return c
+def generateSelect():
+    selectname = input("Please Insert Select Name")
+    c = select(name=selectname,cls="form-control")
+    #c.add(option())
+    return c
+    
+print ("Welcome To HTML Generator :")
+print ("Select Option ")
+print ("Code : 1. label 2.text 3.textarea 4. groupclass")
+print ("0 To Exit")
 
-dv.add(input_("haha",name="test"))
-print(dv)
+option = input("Please Select Option : ")
+
+dv = div()
+
+dv['class'] = 'form-group'
+dv.add(input_("haha",name="test",cls="form-control"))
+
+#print(dv)
 d = div()
 with d:
     attr(id='header')
     attr(cls='form-group')
-
-print(d)
+dv.add(d)
+lbl = generateLabel()
+x = generateInput()
+s = generateSelect()
+#dv.add(lbl)
+#dv.add(x)  
+dv.add(s)
+print(dv)
