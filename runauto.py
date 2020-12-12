984ff3eed6307360d76fe968381c5521a4358ef5
Mohd Noor Azam Mohd Fauzi <azam@mida.gov.my>
2020-12-12 23:49:30 +0000

    Tambah command option dalam bentuk text berbanding code

--- runauto.py
+++ runauto.py
@@ -1,6 +1,21 @@
 import dominate
 import sys
 from dominate.tags import *
+def listcommand():
+    print ("Welcome To HTML Generator :")
+    print ("Select Option ")
+    print ("Code : 1. label 2.text 3.textarea 4.select ")
+    print ("Prese 5 to preview");
+    print ("Code : 1. = create label")
+    print ("Code : 2. = create text")
+    print ("Code : 3. = create textareat")
+    print ("Code : 4. = create select")
+    print ("Code : 5. = preview result")
+    print ("Code : 6. = reset")
+    print ("COde : 7. = save to file")
+    print ("Code : 8. = open new tag")
+    print ("Code : 9. = close tag")
+    print ("Code : 0. = To Exit")
 def generateLabel() :
     lblname = input("Please Insert Label Name")
     x = label(lblname,cls="col-md-2 control-label",_for="text-field")
@@ -18,7 +33,7 @@ def generateSelect():
     return c
 def generateTextarea():
     textareaname = input("Please Insert Textarea Name")
-    c = textarea(name=textareaname,cls="form-control")
+    c = textarea(name=textareaname,id=textareaname,cls="form-control")
     return c
 def generateP():
     selectname = input("Please Insert Select Name")
@@ -30,64 +45,64 @@ print ("Welcome To HTML Generator :")
 print ("Select Option ")
 print ("Code : 1. label 2.text 3.textarea 4.select ")
 print ("Prese 5 to preview");
-print ("Code : 1. label ")
-print ("Code : 2. text")
-print ("Code : 3.textarea 4.select ")
-print ("Code : 4.select ")
-print ("Code : 5.Preview ")
-print ("Code : 6.Reset")
-print ("COde : 7.Save To File")
-print ("Code : 8.Open New DIV Tag")
-print ("Code : 9.Close Old Tag")
-print ("0 To Exit")
+print ("Code : 1. = create label")
+print ("Code : 2. = create text")
+print ("Code : 3. = create textarea")
+print ("Code : 4. = create select")
+print ("Code : 5. = preview result")
+print ("Code : 6. = reset")
+print ("COde : 7. = save to file")
+print ("Code : 8. = open div tag")
+print ("Code : 9. = close tag")
+print ("Code : 0. = To Exit")
 loop_x = 1
 dv = div()
 dv['class'] = 'form-group'
 
 while loop_x == 1:
     option = input("Please Select Option : ")
-    if option == "1":
+    if option == "1" or option == "create label":
         lbl = generateLabel()
         if 'subdiv' in locals():
             print("SUB DIV IS EXIST")
             subdiv.add(lbl)
         else:
             dv.add(lbl)
-    if option == "2":
+    if option == "2" or option == "create text":
         txt = generateInput()
         if 'subdiv' in locals():
             subdiv.add(txt)
             print("SUB DIV IS EXIST")
         else:
             dv.add(txt)
-    if option == "3":
+    if option == "3" or option == "create textarea":
+        txtarea = generateTextarea()
         if 'subdiv' in locals():
             print("SUB DIV IS EXIST")
-            subdiv.add(txt)
+            subdiv.add(txtarea)
         else:
-            textarea = generateTextarea()
-        #print(dv)
-        dv.add(textarea)
-    if option == "4":   
+            dv.add(txtarea)
+    if option == "4" or option == "create select":   
         s = generateSelect() 
         if 'subdiv' in locals():
             print("SUB DIV IS EXIST")
-            subdiv.add(txt)
+            subdiv.add(s)
         else:
             dv.add(s)
-    if option == "5":
+    if option == "5" or option == "preview result":
         print(dv)   
-    if option == "6":
+    if option == "6" or option == "reset":
         dv = div()  
-    if option == "0":
+    if option == "0" or option == "exit":
         loop_x = 0
-    if option == "7":
+    if option == "7" or option == "save to file" :
         str_file = input("What is your file name")
         f = open(str_file, "w+")
+        f.write("\n")
         f.write(str(dv))
         f.close()
-    if option == "8":
+    if option == "8" or option == "open div tag" :
         subdiv = div()
-        subdiv['class']="col-md-4"
+        subdiv['class']="col-md-10"
     if option == "9":
-        dv.add(subdiv)    
+        dv.add(subdiv) 
